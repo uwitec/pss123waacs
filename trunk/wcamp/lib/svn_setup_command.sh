e2e785e4cb116d69f25ce6b6b95bcfd0a22c906e
yamada.masahiro <yamada.masahiro@4e1df32d-e89f-e3c6-e6b8-afb84736e32e>
2010-08-08 06:24:25 +0000

    ajust some.

--- trunk/wcamp/lib/svn_setup_command.sh
+++ trunk/wcamp/lib/svn_setup_command.sh
@@ -6,13 +6,6 @@ svn propset svn:ignore "*.log" log/
 svn ci -m 'ignore log/*.log'
 svn up
 
-svn remove db/development.sqlite3
-svn ci -m 'rm development'
-svn up
-svn propset svn:ignore "*.sqlite3" db/
-svn ci -m 'ignore db/*.sqlite3'
-svn up
-
 svn propset svn:ignore "*" tmp/cache/
 svn propset svn:ignore "*" tmp/sessions/
 svn propset svn:ignore "*" tmp/sockets/
