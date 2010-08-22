ffed9ad00e45eab1b9b13779748751c47529df11
yamada.masahiro <yamada.masahiro@4e1df32d-e89f-e3c6-e6b8-afb84736e32e>
2010-08-22 13:12:13 +0000

    now working.

--- wcamp/app/controllers/ctl_controller.rb
+++ wcamp/app/controllers/ctl_controller.rb
@@ -2,19 +2,28 @@ class CtlController < CaseinController #ApplicationController
 	layout "wcamp"
 	
   def index
+		@work_orders = WorkOrder.find(:all)
 		list		
   end
 
 	def list
-		@work_orders = WorkOrder.find(:all)
 		@active_orders = WorkOrder.active
 	end
 
+	def search
+		if [nil,''].include?(params[:search])
+			@work_orders = WorkOrder.find(:all)
+		else
+			@work_orders = WorkOrder.find(:all, :conditions => {:work_no => params[:search]})
+		end
+		render_page
+	end
+	
 	def show
 		work_order = WorkOrder.find(params[:id].to_i)
 		render :update do |page|
 			page.replace_html "work_order_#{params[:id]}", 
-				render(:partial => "/shared/order_list", :locals => {:work_order => work_order})
+				render(:partial => "order_list", :locals => {:work_order => work_order})
 		end
 	end
 
@@ -22,6 +31,14 @@ class CtlController < CaseinController #ApplicationController
 		work_order = WorkOrder.find(params[:id].to_i)
 		render :update do |page|
 			page.replace_html "work_order_#{work_order.id}", ""
+		end
+	end
+	
+	def render_page
+		list
+		render :update do |page|
+			page.visual_effect :highlight, 'order_list', :duration => 2
+			page.replace_html 'work_order_list', render(:partial => 'work_order_list')
 		end
 	end
 end
