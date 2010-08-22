c7d50b03f15d7825484aaeadadb8cc3d600662e9
yamada.masahiro <yamada.masahiro@4e1df32d-e89f-e3c6-e6b8-afb84736e32e>
2010-08-22 12:52:57 +0000

    working.

--- wcamp/app/models/work_order.rb
+++ wcamp/app/models/work_order.rb
@@ -1,5 +1,14 @@
 class WorkOrder < ActiveRecord::Base
 
+
+	def self.active
+		work_nos = ShipOrder.find(:all,
+			:select => [:work_no], 
+			:conditions => {:status => [:active, :trouble]}, 
+			:order => "start_at")
+		WorkOrder.find(:all, :conditions => {:work_no => work_nos})
+	end
+	
 	def ship_orders
 		ShipOrder.find(:all, :conditions => {:work_no => self.work_no})
 	end
@@ -9,11 +18,11 @@ class WorkOrder < ActiveRecord::Base
 	end
 
 	def status
-		ship_orders.map{|s| s.status}.uniq.compact.join(",")	
+		ship_orders.map{|s| s.status}.uniq.compact.join(" ")	
 	end
 
 	def device
-		ship_orders.map{|s| s.device}.uniq.compact.join(",")	
+		ship_orders.map{|s| s.device}.uniq.compact.join(" ")	
 	end
 
 	def order_lines
