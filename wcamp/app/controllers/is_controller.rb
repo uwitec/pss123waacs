e83bd67964b280e4cb2dc0aef31ca952ac913a73
yamada.masahiro <yamada.masahiro@4e1df32d-e89f-e3c6-e6b8-afb84736e32e>
2010-08-24 03:54:38 +0000

    now working.

--- wcamp/app/controllers/is_controller.rb
+++ wcamp/app/controllers/is_controller.rb
@@ -1,9 +1,18 @@
 class IsController < ApplicationController
   def index
-		@title = "テスト"
+		@work_orders = WorkOrder.find(:all)
+		@title = "WAACS for iPhone"
   end
 
 	def clock
 		@title = "テスト"
+	end
+
+	def picking
+		orders = WorkOrder.find(params[:id].to_i).ship_orders
+		render :update do |page|
+			page.replace_html 'work', 
+				render(:partial => 'order_list', :locals => {:orders => orders})
+		end
 	end
 end
