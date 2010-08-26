edd7396c6941e70278eeb3ec7b94a12b1b4c7201
yamada.masahiro <yamada.masahiro@4e1df32d-e89f-e3c6-e6b8-afb84736e32e>
2010-08-26 08:54:48 +0000

    ajust navagation.

--- trunk/wcamp/app/helpers/application_helper.rb
+++ trunk/wcamp/app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+	def show_time date
+		[nil,""].include?(date) ? "-" : date.strftime("%Y-%m-%d %H:%M")
+	end
+
+	def show_date date
+		[nil,""].include?(date) ? "-" : date.strftime("%Y-%m-%d")
+	end
+
+	def link_menu_to(name, options = {}, html_options ={})
+		html_options = {:class => 'active'}.merge(html_options) if current_page?(options)
+		link_to name, options, html_options
+	end
 end
