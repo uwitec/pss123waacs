7a427dbe763f4f18113d3c0897f8172137d89ec2
yamada.masahiro <yamada.masahiro@4e1df32d-e89f-e3c6-e6b8-afb84736e32e>
2010-08-26 06:18:48 +0000

    now edi_monitor working.

--- wcamp/app/controllers/edi_controller.rb
+++ wcamp/app/controllers/edi_controller.rb
@@ -1,6 +1,10 @@
 class EdiController < CaseinController #ApplicationController
 	layout 'wcamp'
   def index
+		list
   end
 
+	def list
+		@edi_files = EdiFile.find(:all)		
+	end
 end
--- wcamp/app/models/edi_file.rb
+++ wcamp/app/models/edi_file.rb
@@ -1,2 +1,22 @@
 class EdiFile < ActiveRecord::Base
+	EDI_TYPE = [
+		%w(出荷指示 shipping),
+		%w(出荷実績 shipped),
+		%w(入荷予定 receiving),
+		%w(入荷実績 received),
+		%w(棚卸計画 stock_keeping),
+		%w(在庫一覧 inventries)
+	]
+
+	def self.status tag
+		'Unknown'
+	end
+
+	def self.last_edi_status tag
+		'Unknown last'
+	end
+
+	def self.last_edi_at tag
+		'anytime'
+	end
 end
