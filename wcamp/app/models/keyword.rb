6706add5876a2e6f637bcd679ea84abc1ebf98d1
yamada.masahiro <yamada.masahiro@4e1df32d-e89f-e3c6-e6b8-afb84736e32e>
2010-08-28 06:46:30 +0000

    now working

---
+++ wcamp/app/models/keyword.rb
@@ -0,0 +1,31 @@
+class Keyword
+	KEYS = (
+		%w(search)
+	).freeze
+	
+	KEYS.each{|key| eval("attr_accessor :#{key}")}
+
+	def set params
+		KEYS.each{|key| eval("@#{key} = params[:#{key}] if params[:#{key}]")}
+	end
+
+	def reset
+		KEYS.each{|key| eval("@#{key} = ''")}
+	end
+
+	def search_receive_conditions
+		conditions = []
+		unless [nil,''].include?(self.search)
+			joined_col_name = 'work_no || goods_code'
+			searchs = Array.new
+			searchs = self.search.to_s.split(/&/)	
+			searchs.each do |word|
+				search = Array.new
+				search << word.to_s.strip.split(/ /)
+				search = (search.delete_if{|w| w.empty?}).map.join("%")
+				conditions.push(joined_col_name + " like '%#{search}%'")
+			end
+		end
+		conditions.join(" and ")
+	end	
+end
