c44bf1e3d59ab3aed5130e0484c5b14341cc6c02
yamada.masahiro <yamada.masahiro@4e1df32d-e89f-e3c6-e6b8-afb84736e32e>
2010-09-20 03:05:35 +0000

    add rss feeder

---
+++ wcamp/app/models/info_catcher.rb
@@ -0,0 +1,36 @@
+class InfoCatcher
+	require 'rss'
+	require 'uri'
+	attr_accessor :title, :description, :link, :published_at
+	
+	def initialize
+		@title = nil
+		@description = nil
+		@link = nil
+		@published_at = nil
+	end
+
+	def self.get_rss(rss_url)
+		begin
+			ary = []
+			unless rss_url.nil?
+				url = URI.parse(rss_url).normalize
+				open(url) do |http|
+					response = http.read
+					rss_result = RSS::Parser.parse(response, false)
+					rss_result.items.each do |item|
+						obj = self.new
+						obj.title = item.title
+						obj.description = item.content
+						obj.link = item.link
+						obj.published_at = item.date
+						ary << obj
+					end
+				end
+			end
+			return ary
+		rescue => e
+			raise e
+		end
+	end
+end
