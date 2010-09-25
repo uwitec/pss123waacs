7d6f4fd08e484dcf352437ebb102815417e6de99
yamada.masahiro <yamada.masahiro@4e1df32d-e89f-e3c6-e6b8-afb84736e32e>
2010-09-25 23:07:57 +0000

    add shipping address master mente

--- wbirdfarm/app/models/inventory.rb
+++ wbirdfarm/app/models/inventory.rb
@@ -1,6 +1,8 @@
 class Inventory < ActiveRecord::Base
 	belongs_to :ware_house
 
+	PICKING_STYLE = %w(single total DAS CART).freeze
+
 	def show_ware_house_name
 		self.ware_house.nil? ? '-' : self.ware_house.name
 	end
--- wbirdfarm/app/models/shipping_address.rb
+++ wbirdfarm/app/models/shipping_address.rb
@@ -1,4 +1,8 @@
 class ShippingAddress < ActiveRecord::Base
 	has_many :orders
 	belongs_to :ware_house
+
+	def show_ware_house_name
+		self.ware_house.nil? ? '-' : self.ware_house.name
+	end
 end
