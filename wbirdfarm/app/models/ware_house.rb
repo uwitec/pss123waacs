d66b6d2c8da66d1d9d832086424f138def92e7f5
yamada.masahiro <yamada.masahiro@4e1df32d-e89f-e3c6-e6b8-afb84736e32e>
2010-09-25 07:33:48 +0000

    add master mente for region, ware house.

--- wbirdfarm/app/models/ware_house.rb
+++ wbirdfarm/app/models/ware_house.rb
@@ -3,4 +3,10 @@ class WareHouse < ActiveRecord::Base
 	has_many :orders
 	has_many :shipping_addresses
 	belongs_to :region	
+
+	validates_uniqueness_of :code
+
+	def show_region_name
+		self.region.nil? ? '-' : self.region.name
+	end
 end
