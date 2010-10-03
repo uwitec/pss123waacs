c96c40971229e2d9bbfd177ead49c16e49a289d1
yamada.masahiro <yamada.masahiro@4e1df32d-e89f-e3c6-e6b8-afb84736e32e>
2010-10-03 13:10:55 +0000

    now working.

--- trunk/wbirdfarm/app/models/order.rb
+++ trunk/wbirdfarm/app/models/order.rb
@@ -3,4 +3,8 @@ class Order < ActiveRecord::Base
 	belongs_to :shipping_address
 
 	validates_uniqueness_of :goods_code, :scope => [:order_no]
+
+	def show_ware_house_name
+		self.ware_house.nil? ? '-' : self.ware_house.name
+	end
 end
