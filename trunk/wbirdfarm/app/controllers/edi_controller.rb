b6b96654d6cccc79b639a58d91353ddb70a590bf
yamada.masahiro <yamada.masahiro@4e1df32d-e89f-e3c6-e6b8-afb84736e32e>
2010-10-17 13:11:07 +0000

    just now proto of zaiko-shokai and EDI screen.

--- trunk/wbirdfarm/app/controllers/edi_controller.rb
+++ trunk/wbirdfarm/app/controllers/edi_controller.rb
@@ -6,6 +6,25 @@ class EdiController < CaseinController
 
 	def list
 		@ware_houses = WareHouse.all
+		@edi_files = EdiFile.all
 	end
 
+	def refresh
+		render_page
+	end
+
+	def edi_start
+		eval("EdiFile.#{params[:tag]}")
+		render_page
+	end
+
+	def render_page
+		list
+		render :update do |page|
+			page.visual_effect :highlight, 'edi_class_list', :duration => 2
+			page.replace_html 'edi_class_list', render(:partial => 'edi_class_list')	
+			page.visual_effect :highlight, 'edi_file_list', :duration => 2
+			page.replace_html 'edi_file_list', render(:partial => 'edi_file_list')	
+		end	
+	end
 end
