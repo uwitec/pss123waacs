0dd9382711d18bc6b5bd7bd5e0d146b8af3ea8d8
yamada.masahiro <yamada.masahiro@4e1df32d-e89f-e3c6-e6b8-afb84736e32e>
2010-10-17 13:11:07 +0000

    just now proto of zaiko-shokai and EDI screen.

--- wbirdfarm/app/models/edi_file.rb
+++ wbirdfarm/app/models/edi_file.rb
@@ -8,4 +8,26 @@ class EdiFile < ActiveRecord::Base
 		%w(棚卸計画 stock_keeping),
 		%w(在庫一覧 inventories)
 	].freeze
+
+	def self.show_status tag
+		'-'
+	end
+
+	def self.lines tag
+		[]
+	end
+
+	def self.last_edi_at tag
+		nil
+	end
+
+	# EDI
+	def self.ordering
+		Edi.new.receive_order
+	end
+	
+	def self.shipping
+		PickingPlan.allocate_total
+		PickingPlan.allocate_single
+	end
 end
