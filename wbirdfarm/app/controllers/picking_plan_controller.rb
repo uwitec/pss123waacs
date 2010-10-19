97d10274215eade912b7b64866cd63cbfe7ab9e8
yamada.masahiro <yamada.masahiro@4e1df32d-e89f-e3c6-e6b8-afb84736e32e>
2010-10-19 09:52:33 +0000

    add reset_allocate function

--- wbirdfarm/app/controllers/picking_plan_controller.rb
+++ wbirdfarm/app/controllers/picking_plan_controller.rb
@@ -9,4 +9,17 @@ class PickingPlanController < CaseinController
 		@ware_houses = WareHouse.all
 	end
 
+	def reset_allocate
+		PickingPlan.allocated_clear
+		render_page	
+	end
+
+	def render_page
+		list
+		render :update do |page|
+			page.visual_effect :highlight , "inv_control"
+			page.replace_html 'inv_control', render(:partial => 'inv_control')
+		end
+	end
+
 end
