4b797667e0414fc944e3f9af4c6b56ced6bb01cb
yamada.masahiro <yamada.masahiro@4e1df32d-e89f-e3c6-e6b8-afb84736e32e>
2010-10-24 12:00:03 +0000

    add trial function on edi_controller

--- trunk/wbirdfarm/app/controllers/picking_plan_controller.rb
+++ trunk/wbirdfarm/app/controllers/picking_plan_controller.rb
@@ -2,6 +2,8 @@ class PickingPlanController < CaseinController
 	layout "bf"
 
   def index
+		@picking_style = ''
+		@customer_code = ''
 		list
   end
 
@@ -14,6 +16,30 @@ class PickingPlanController < CaseinController
 		render_page	
 	end
 
+	def allocate_single
+		PickingPlan.new.allocate_single
+		render_page	
+	end
+
+	def allocate_total
+		PickingPlan.new.allocate_total
+		render_page	
+	end
+
+	def change_picking_style
+		shipping_addresses = ShippingAddress.all(:conditions => {:customer_code => params[:customer_code]})
+		@customer_code = params[:customer_code]
+		@picking_style = shipping_addresses.map{|s| s.picking_style }.uniq.compact[0]
+		unless [nil,''].include?(params[:picking_style])
+			shipping_addresses.each do |sa|
+				sa.picking_style = params[:picking_style]
+				sa.save
+			end
+		end
+		@picking_style = shipping_addresses.map{|s| s.picking_style}.uniq.compact[0]
+		render_page	
+	end
+
 	def render_page
 		list
 		render :update do |page|
@@ -21,5 +47,4 @@ class PickingPlanController < CaseinController
 			page.replace_html 'inv_control', render(:partial => 'inv_control')
 		end
 	end
-
 end
